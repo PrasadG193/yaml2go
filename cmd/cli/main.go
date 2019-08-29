c15b4876a7cdd678d5957d6276986e6aa39c1321
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-08-29 12:30:33 +0530

    Separate yaml2go and main package
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

---
+++ cmd/cli/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/PrasadG193/yaml2go"
+)
+
+const helpMsg = `yaml2go converts YAML specs to Go type definitions
+
+Usage:
+    yaml2go < /path/to/yamlspec.yaml
+
+Examples:
+    yaml2go < test/example1.yaml
+    yaml2go < test/example1.yaml > example1.go
+`
+
+func printHelp(f string) {
+	helpArgs := []string{"-h", "--help", "help"}
+	for _, m := range helpArgs {
+		if f == m {
+			fmt.Println(helpMsg)
+			os.Exit(0)
+		}
+	}
+}
+
+func main() {
+	// Read args
+	if len(os.Args) > 1 {
+		printHelp(os.Args[1])
+	}
+
+	// Read input from the console
+	var data string
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		data += scanner.Text() + "\n"
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error while reading input:", err)
+	}
+
+	// Create yaml2go object and invoke Convert()
+	y2g := yaml2go.New()
+	result := y2g.Convert("Yaml2Go", []byte(data))
+
+	fmt.Printf(result)
+	return
+}
