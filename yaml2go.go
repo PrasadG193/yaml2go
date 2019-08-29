5c23197573a9258da478114a0959fc47c60376a6
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-08-29 12:30:33 +0530

    Separate yaml2go and main package
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- yaml2go.go
+++ yaml2go.go
@@ -1,85 +1,27 @@
-package main
+package yaml2go
 
 import (
-	"bufio"
 	"fmt"
 	"go/format"
 	"gopkg.in/yaml.v2"
 	"log"
-	"os"
 	"reflect"
 	"strings"
 )
 
-const helpMsg = `yaml2go converts YAML specs to Go type definitions
-
-Usage:
-    yaml2go < /path/to/yamlspec.yaml
-
-Examples:
-    yaml2go < test/example1.yaml
-    yaml2go < test/example1.yaml > example1.go
-`
+// New creates Yaml2Go object
+func New() Yaml2Go {
+	return Yaml2Go{}
+}
 
 // Yaml2Go to store converted result
 type Yaml2Go struct {
-	//Result    string
 	StructMap map[string]string
 }
 
 // AppendResult add lines to the result
 func (yg *Yaml2Go) AppendResult(structName string, line string) {
 	yg.StructMap[structName] += line
-}
-
-func printHelp(f string) {
-	helpArgs := []string{"-h", "--help", "help"}
-	for _, m := range helpArgs {
-		if f == m {
-			fmt.Println(helpMsg)
-			os.Exit(0)
-		}
-	}
-}
-
-func main() {
-	// Read args
-	if len(os.Args) > 1 {
-		printHelp(os.Args[1])
-	}
-
-	// Read input from the console
-	var data string
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		data += scanner.Text() + "\n"
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal("Error while reading input:", err)
-	}
-
-	// Unmarshal to map[string]interface{}
-	var t map[string]interface{}
-	err := yaml.Unmarshal([]byte(data), &t)
-	if err != nil {
-		log.Fatal("Failed to parse input")
-	}
-
-	y2g := Yaml2Go{}
-	y2g.StructMap = make(map[string]string)
-	y2g.Convert("Yaml2Go", t)
-
-	var result string
-	for _, value := range y2g.StructMap {
-		result += fmt.Sprintf("%s\n", value)
-	}
-	// Convert result into go format
-	goFormat, err := format.Source([]byte(result))
-	if err != nil {
-		log.Fatal("go fmt error:", err)
-	}
-	fmt.Printf(string(goFormat))
-	return
 }
 
 // removeUnderscores and camelize string
@@ -96,13 +38,33 @@ func goKeyFormat(key string) string {
 }
 
 // Convert transforms map[string]interface{} to go struct
-func (yg *Yaml2Go) Convert(structName string, obj map[string]interface{}) {
+func (yg *Yaml2Go) Convert(structName string, data []byte) string {
+	yg.StructMap = make(map[string]string)
+
+	// Unmarshal to map[string]interface{}
+	var obj map[string]interface{}
+	err := yaml.Unmarshal(data, &obj)
+	if err != nil {
+		log.Fatal("Failed to parse input")
+	}
+
 	yg.AppendResult("Yaml2Go", "// Yaml2Go\n")
 	yg.AppendResult("Yaml2Go", "type Yaml2Go struct {\n")
 	for k, v := range obj {
 		yg.Structify(structName, k, v, false)
 	}
 	yg.AppendResult("Yaml2Go", "}\n")
+
+	var result string
+	for _, value := range yg.StructMap {
+		result += fmt.Sprintf("%s\n", value)
+	}
+	// Convert result into go format
+	goFormat, err := format.Source([]byte(result))
+	if err != nil {
+		log.Fatal("go fmt error:", err)
+	}
+	return string(goFormat)
 }
 
 // Structify transforms map key values to struct fields
--- yaml2go_test.go
+++ yaml2go_test.go
@@ -1,9 +1,7 @@
-package main
+package yaml2go
 
 import (
 	"fmt"
-	"go/format"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"strings"
@@ -11,31 +9,19 @@ import (
 )
 
 func conversion(data []byte) string {
-	var d map[string]interface{}
-	err := yaml.Unmarshal(data, &d)
-	if err != nil {
-		log.Fatal("Failed to parse input")
-	}
+	y2g := New()
+	result := y2g.Convert("Yaml2Go", data)
 
-	y2g := Yaml2Go{}
-	y2g.AppendResult("type Yaml2Go struct {\n")
-	y2g.Convert(d)
-	y2g.AppendResult("}")
-
-	// Convert result into go format
-	_, err = format.Source([]byte(y2g.Result))
-	if err != nil {
-		log.Fatal("go fmt error:", err)
-	}
-	return string(y2g.Result)
+	return result
 }
 
 func compareResults(actual []string, expected map[string]bool) error {
-	if len(actual) != len(expected) {
-		return fmt.Errorf("incomplete conversion")
-	}
 	for _, r := range actual {
+		if len(r) == 0 {
+			continue
+		}
 		if ok, _ := expected[r]; !ok {
+			fmt.Printf("Couldn't find: %s %d\n", r, len(r))
 			return fmt.Errorf("incomplete conversion")
 		}
 	}
@@ -45,36 +31,40 @@ func compareResults(actual []string, expected map[string]bool) error {
 func TestConvert(t *testing.T) {
 	expected := map[string]map[string]bool{
 		"example1": map[string]bool{
-			"type Yaml2Go struct {":                      true,
-			"Array3 []float64 `yaml:\"array3\"`":         true,
-			"Kind string `yaml:\"kind\"`":                true,
-			"Metadata struct {":                          true,
-			"Name string `yaml:\"name\"`":                true,
-			"Nullfield interface{} `yaml:\"nullfield\"`": true,
-			"Nestedstruct []struct {":                    true,
-			"Nested struct {":                            true,
-
-			"UnderscoreField string `yaml:\"underscore_field\"`": true,
-
-			"Field1 []float64 `yaml:\"field1\"`":         true,
-			"Field2 []bool `yaml:\"field2\"`":            true,
-			"} `yaml:\"nested\"`":                        true,
-			"Nested2 []struct {":                         true,
-			"Nested3 struct {":                           true,
-			"Field1 []int `yaml:\"field1\"`":             true,
-			"Fieldt []bool `yaml:\"fieldt\"`":            true,
-			"Field3 string `yaml:\"field3\"`":            true,
-			"} `yaml:\"nested3\"`":                       true,
-			"} `yaml:\"nested2\"`":                       true,
-			"} `yaml:\"nestedstruct\"`":                  true,
-			"} `yaml:\"metadata\"`":                      true,
-			"Abc []struct {":                             true,
-			"Def []string `yaml:\"def\"`":                true,
-			"} `yaml:\"abc\"`":                           true,
-			"Array1 []string `yaml:\"array1\"`":          true,
-			"Array2 []int `yaml:\"array2\"`":             true,
-			"IsUnderscore bool `yaml:\"is_underscore\"`": true,
-
+			"// Yaml2Go":            true,
+			"type Yaml2Go struct {": true,
+			"	Kind         string    `yaml:\"kind\"`": true,
+			"	Metadata     Metadata  `yaml:\"metadata\"`": true,
+			"	Abc          []Abc     `yaml:\"abc\"`": true,
+			"	Array1       []string  `yaml:\"array1\"`": true,
+			"	Array2       []int     `yaml:\"array2\"`": true,
+			"	Array3       []float64 `yaml:\"array3\"`": true,
+			"	IsUnderscore bool      `yaml:\"is_underscore\"`": true,
+			"// Metadata":            true,
+			"type Metadata struct {": true,
+			"	Name         string         `yaml:\"name\"`": true,
+			"	Nullfield    interface{}    `yaml:\"nullfield\"`": true,
+			"	Nestedstruct []Nestedstruct `yaml:\"nestedstruct\"`": true,
+			"// Nested3":            true,
+			"type Nested3 struct {": true,
+			"	Field1 []int  `yaml:\"field1\"`": true,
+			"	Fieldt []bool `yaml:\"fieldt\"`": true,
+			"	Field3 string `yaml:\"field3\"`": true,
+			"// Abc":            true,
+			"type Abc struct {": true,
+			"	Def []string `yaml:\"def\"`": true,
+			"// Nestedstruct":            true,
+			"type Nestedstruct struct {": true,
+			"	Nested  Nested    `yaml:\"nested\"`": true,
+			"	Nested2 []Nested2 `yaml:\"nested2\"`": true,
+			"// Nested":            true,
+			"type Nested struct {": true,
+			"	UnderscoreField string    `yaml:\"underscore_field\"`": true,
+			"	Field1          []float64 `yaml:\"field1\"`": true,
+			"	Field2          []bool    `yaml:\"field2\"`": true,
+			"// Nested2":            true,
+			"type Nested2 struct {": true,
+			"	Nested3 Nested3 `yaml:\"nested3\"`": true,
 			"}": true,
 		},
 	}
