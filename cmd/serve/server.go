536a79e9c02f5f32d21aabc800bc829c51734b96
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-10-14 19:34:25 +0530

    move handler function to yaml2go pkg
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- cmd/serve/server.go
+++ cmd/serve/server.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -19,31 +17,9 @@ const PORT = "8080"
 func main() {
 	router := httprouter.New()
 	log.Printf("server started accepting requests on port=%s..\n", PORT)
-	router.POST(fmt.Sprintf("/%s/convert", API_VERSION), handleConvert)
+	router.POST(fmt.Sprintf("/%s/convert", API_VERSION), yaml2go.HandleConvert)
 
 	handler := cors.Default().Handler(router)
 	log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
-func handleConvert(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	// Enable CORS
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, fmt.Sprintf("Bad Request. Error: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-
-	// Create yaml2go object and invoke Convert()
-	y2g := yaml2go.New()
-	result, err := y2g.Convert("Yaml2Go", []byte(data))
-	if err != nil {
-		log.Println(err)
-		http.Error(w, fmt.Sprintf("Bad Request. Error: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	io.WriteString(w, result)
-}
