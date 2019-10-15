a9bee877b6785736e08a73f77292ee767c7bc9e1
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-10-15 15:51:21 +0530

    Move http req handler to separate file
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- cmd/serve/server.go
+++ cmd/serve/server.go
@@ -6,20 +6,13 @@ import (
 	"net/http"
 
 	"github.com/PrasadG193/yaml2go"
-
-	"github.com/julienschmidt/httprouter"
-	"github.com/rs/cors"
 )
 
 const API_VERSION = "v1"
 const PORT = "8080"
 
 func main() {
-	router := httprouter.New()
 	log.Printf("server started accepting requests on port=%s..\n", PORT)
-	router.POST(fmt.Sprintf("/%s/convert", API_VERSION), yaml2go.HandleConvert)
-
-	handler := cors.Default().Handler(router)
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	http.HandleFunc(fmt.Sprintf("/%s/convert", API_VERSION), yaml2go.HandleConvert)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
--- yaml2go.go
+++ yaml2go.go
@@ -6,10 +6,6 @@ import (
 	"gopkg.in/yaml.v2"
 	"reflect"
 	"strings"
-	"io"
-	"log"
-	"io/ioutil"
-	"net/http"
 )
 
 // New creates Yaml2Go object
@@ -142,28 +138,4 @@ func (yg *Yaml2Go) Structify(structName, k string, v interface{}, arrayElem bool
 	default:
 		yg.AppendResult(structName, fmt.Sprintf("%s %s `yaml:\"%s\"`\n", goKeyFormat(k), reflect.TypeOf(v).String(), k))
 	}
-}
-
-
-func HandleConvert(w http.ResponseWriter, r *http.Request) {
-	// Enable CORS
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, fmt.Sprintf("Bad Request. Error: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-
-	// Create yaml2go object and invoke Convert()
-	y2g := New()
-	result, err := y2g.Convert("Yaml2Go", []byte(data))
-	if err != nil {
-		log.Println(err)
-		http.Error(w, fmt.Sprintf("Bad Request. Error: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	io.WriteString(w, result)
 }
